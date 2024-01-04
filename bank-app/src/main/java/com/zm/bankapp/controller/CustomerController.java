7c9fa423c15124eb20f0299837c348f913fbdb2c
S Jaya Krishna <S Jaya Krishna@ZML-LP59>
2024-01-04 08:45:46 +0530

    working fine

--- bank-app/src/main/java/com/zm/bankapp/controller/CustomerController.java
+++ bank-app/src/main/java/com/zm/bankapp/controller/CustomerController.java
@@ -1,10 +1,14 @@
 package com.zm.bankapp.controller;
 
 import java.io.IOException;
-import java.math.BigDecimal;
+import java.util.List;
 
+import com.zm.bankapp.dto.BankTransaction;
 import com.zm.bankapp.service.AccountServiceImpl;
+import com.zm.bankapp.service.BankTransactionService;
+import com.zm.bankapp.service.BankTransactionServiceImpl;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -27,7 +31,14 @@ public class CustomerController extends HttpServlet{
 			session.setAttribute("bal", "Account Balance is  " + bal);
 			req.getRequestDispatcher("customer-dashboard.jsp").forward(req, resp);
 		}if(action.equalsIgnoreCase("tx")) {
-			
+			BankTransactionService bankService = new BankTransactionServiceImpl();
+			Integer acc = (Integer)session.getAttribute("onlyAcc");
+			Integer accountNo = acc;
+			List<BankTransaction> transactionHistory = bankService.getTransactionHistoryByAccountNo(accountNo);
+	         req.setAttribute("transList",transactionHistory);
+	         resp.setContentType("text/html");
+			 RequestDispatcher rd = req.getRequestDispatcher("tx-history-customer.jsp");
+			 rd.forward(req, resp);
 		}
 	}
 	
